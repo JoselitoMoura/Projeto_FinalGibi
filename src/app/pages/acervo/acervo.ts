bfa4db26f2013bd01bd6c2bf126d58eca8067a95
JoselitoMoura <joselitodemourabarbosa>
2026-01-14 13:40:02 +0000

    Criação dos Componentes e das Pages

---
+++ src/app/pages/acervo/acervo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acervo',
+  imports: [],
+  templateUrl: './acervo.html',
+  styleUrl: './acervo.css',
+})
+export class Acervo {
+
+}
