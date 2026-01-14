30c4f37b6f109e32836556c32b183a4a0fddbc2e
JoselitoMoura <joselitodemourabarbosa>
2026-01-14 21:24:22 +0000

    Alterado o arquivo app.routes.ts; Criados e preenchidos os arquivos header.component.html e header.component.ts .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AcervoComponent } from './pages/acervo/acervo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'acervo', component: AcervoComponent }
+];
